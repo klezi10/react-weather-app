3aa36bac1ea0bd3e392234532179d1780f59e04b
Klesta Luli <74952593+klezi10@users.noreply.github.com>
2022-07-18 12:38:08 +0400

    deleted footer

--- src/App.js
+++ src/App.js
@@ -7,25 +7,6 @@ export default function App() {
     <div className="App">
       <div className="container">
         <Weather defaultCity="Toronto" />
-        <footer>
-          This project was coded by{' '}
-          <a
-            href="https://clever-turing-3f9e94.netlify.app/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            {' '}
-            Klesta Luli
-          </a>{' '}
-          |{' '}
-          <a
-            href="https://github.com/klezi10/react-weather-app"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Open-source
-          </a>{' '}
-        </footer>
       </div>
     </div>
   );
